913111abfcf8e64a44a4f03455afa10d94dc8ae2
max <mpflueger@users.noreply.github.com>
2023-04-01 21:03:16 -0700

    Add ThreadPool class and threaded opening search.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ INCLUDE =
 all: wordle_genie
 
 wordle_genie: wordle_genie.cc
-	g++ -O3 --std=c++1z $(INCLUDE) wordle_genie.cc -o wordle_genie
+	g++ -O3 --std=c++17 $(INCLUDE) wordle_genie.cc -o wordle_genie
 
 clean:
 	rm wordle_genie
--- wordle_genie.cc
+++ wordle_genie.cc
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 #include <chrono>
+#include <condition_variable>
 #include <fstream>
 #include <iostream>
 #include <mutex>
@@ -10,8 +11,10 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <queue>
 
 using namespace std;
+using namespace std::chrono_literals;
 
 struct Guess {
   string word;
@@ -132,37 +135,116 @@ vector<int> evaluateOpen(
   return distribution;
 }
 
+void safeEvaluateOpen(
+    const vector<string>& guesses,
+    const vector<string>& word_list,
+    std::mutex& result_mutex,
+    EvalResult& eval_result
+    //vector<int>& distribution,
+    ) {
+  // cout << "Entering safeEvaluateOpen for guesses: " << guesses[0] << endl;
+  EvalResult local_result;
+  std::vector<int> local_dist;
+  local_dist = evaluateOpen(guesses, word_list, local_result);
+
+  const std::lock_guard<std::mutex> lock(result_mutex);
+  eval_result = local_result;
+  //distribution = local_dist;
+  // cout << "Leaving safeEvaluateOpen for guesses: " << guesses[0] << endl;
+}
+
 class ThreadPool {
  public:
-  TheadPool(int threads) {
-    for(int i = 0; i < threads; i++) {
-      pool.push_back(std::thread(loop));
+  ThreadPool(int threads) {
+    const std::lock_guard<std::mutex> lock(job_queue_mutex);
+    ok_to_run = true;
+    thread_idle = vector<bool>(threads, true);
+    for (int i = 0; i < threads; i++) {
+      pool.push_back(std::thread(&ThreadPool::loop, this, i));
     }
   }
 
+  ~ThreadPool() {
+    // Tell threads to stop.
+    {
+      const std::lock_guard<std::mutex> lock(job_queue_mutex);
+      ok_to_run = false;
+    }
+    cv.notify_all();
+
+    // Join all threads.
+    for(auto& thread : pool) {
+      thread.join();
+    }
+  }
+
+  ThreadPool(const ThreadPool&) = delete;
+  ThreadPool& operator=(const ThreadPool&) = delete;
+
   void add_job(std::function<void()> job) {
-    // Lock the job queue, release automatically when going out of scope.
-    const std::lock_guard<std::mutex> lock(job_mutex);
-    jobs.push(job);
+    {
+      // Lock the job queue, release automatically when going out of scope.
+      const std::lock_guard<std::mutex> lock(job_queue_mutex);
+      job_queue.push(job);
+    }
+    cv.notify_one();
+  }
+
+  // Find the number of threads currently doing work.
+  int workers_busy() {
+    const std::lock_guard<std::mutex> lock(job_queue_mutex);
+    int busy = 0;
+    for (auto i : thread_idle) {
+      if (!i) busy++;
+    }
+    return busy;
+  }
+
+  /// Return the sum the jobs in process and not started.
+  int jobs_remaining() {
+    const std::lock_guard<std::mutex> lock(job_queue_mutex);
+    int busy = 0;
+    for (auto i : thread_idle) {
+      if (!i) busy++;
+    }
+    return busy + job_queue.size();
   }
 
  private:
-  void loop() {
-    while(still ok to run) {
-      {
-        const std::lock_guard<std::mutex> lock(job_mutex);
-        // TODO Wait for job on queue.
-        auto job = queue.front();
-        queue.pop()
+  void loop(int id) {
+    // Acquire lock.
+    std::unique_lock<std::mutex> lock(job_queue_mutex);
+
+    while(true) {
+      // Check if jobs in queue, if so take one and run.
+      while (!job_queue.empty()) {
+        // Check if ok to run, else return.
+        if (!ok_to_run) return;
+
+        auto job = job_queue.front();
+        job_queue.pop();
+
+        // Do the work, and don't hold the lock while working.
+        thread_idle[id] = false;
+        lock.unlock();
+        job();
+        lock.lock();
+        thread_idle[id] = true;
       }
-      job();
+
+      // Wait on condition variable, but check if we are still ok to run first.
+      if (!ok_to_run) return;
+      cv.wait(lock);
     }
   }
 
   std::vector<std::thread> pool;
-  std::mutex job_mutex;
-  std::queue<std::function<void()>> jobs;
-}
+  std::mutex job_queue_mutex;
+  std::vector<bool> thread_idle;
+  std::condition_variable cv;
+  bool ok_to_run;
+  std::queue<std::function<void()>> job_queue;
+};
 
 void worker(int& next_work, mutex& m, const vector<string>& word_list) {
   while(true) {
@@ -198,6 +280,8 @@ int main(int argc, char** argv) {
   f.close();
   cout << "  Read in " << answer_list.size() << " words." << endl;
 
+  // ==================================================================
+  // Find available next guess options.
   // Run the guesses against the answer.
   vector<string> options;
   // cout << "Running numOptions for TRACE LOINS on ABBEY." << endl;
@@ -217,7 +301,8 @@ int main(int argc, char** argv) {
     cout << word << endl;
   }
 
-  // Evaluate the open.
+  // ===================================================================
+  // Evaluate the opening.
   // cout << "Running evaluateOpen for TRACE LOINS." << endl;
   cout << "Running evaluateOpen for [";
   for (auto& w : guesses) {
@@ -240,12 +325,43 @@ int main(int argc, char** argv) {
   cout << "  Mean: " << eval_result.mean
        << "  Median: " << eval_result.median << endl;
 
-  // TODO: Find the best single word open.
-  mutex worker_mutex;
-  int thread_count = 7;
-  for (int i = 0; i < thread_count; i++) {
-
+  // =================================================================
+  // Find the best single word open.
+  int thread_count = 7; // TODO: set by argument
+  ThreadPool thread_pool(thread_count);
+  mutex result_mutex;
+  unique_lock<mutex> result_lock(result_mutex);
+  condition_variable cv;
+  vector<EvalResult> results(answer_list.size());
+  for (int i = 0; i < answer_list.size(); i++) {
+    // evaluate open with guess
+    thread_pool.add_job([&open = answer_list[i], &answer_list, &result_mutex, &result = results[i]](){
+      safeEvaluateOpen({open}, answer_list, result_mutex, result);});
   }
+
+  while (true) {
+    cv.wait_for(result_lock, 500ms);
+    int remaining = thread_pool.jobs_remaining();
+    cout << "Thread pool has " << remaining << " jobs remaining." << endl;
+    if (remaining == 0) break;
+  }
+
+  int best_index = 0;
+  EvalResult best_result;
+  if (results.size() > 0) {
+    EvalResult best_result = results[0];
+  } else {
+    cerr << "ERROR: no best open results." << endl;
+  }
+  for (int i = 1; i < results.size(); i++) {
+    if (results[i].mean < best_result.mean) {
+      best_index = i;
+      best_result = results[i];
+    }
+  }
+
+  cout << "Best Opening: " << answer_list[best_index] << endl;
+  cout << "  with mean: " << best_result.mean << " and median: " << best_result.median << endl;
 
   return 0;
 }
